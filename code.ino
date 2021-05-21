5d6df0063484b76427bc2bdeb6e3e27c307a5724
Joseph Adewole <38891254+JosephAdewole@users.noreply.github.com>
2021-05-21 08:21:44 +0100

    Updated code.ino

--- code.ino
+++ code.ino
@@ -1,8 +1,10 @@
-
+#include <MsTimer2.h>
 
 int vSpeed = 110;        // MAX 255
 int turn_speed = 230;    // MAX 255 
 int turn_delay = 10;
+bool is_take_picture = false;
+
 
 //L293 Connection   
 const int motorA1      = 10;
@@ -19,7 +21,22 @@ const int right_sensor_pin =A1;
 int left_sensor_state;
 int right_sensor_state;
 
-void setup() {
+void change(){
+  is_take_picture = !is_take_picture;
+  Serial.println("stop motion for picture");
+  digitalWrite(motorA2,LOW);
+  digitalWrite(motorA1,LOW);                       
+  digitalWrite(motorB2,LOW);
+  digitalWrite(motorB1,LOW);
+    MsTimer2::stop();
+}
+
+
+void setup(){
+  
+  MsTimer2::set(3000, change); // 500ms period
+  MsTimer2::start();
+  
   pinMode(motorA1, OUTPUT);
   pinMode(motorA2, OUTPUT);
   pinMode(motorB1, OUTPUT);
@@ -27,26 +44,34 @@ void setup() {
   
   pinMode(takepicture, OUTPUT);
   pinMode(moverobot, INPUT);
+  digitalWrite(moverobot, LOW);
+  digitalWrite(takepicture, HIGH);
 
   Serial.begin(9600);
   delay(3000);
 }
 
 void loop() {
+    while(is_take_picture){
+      digitalWrite(takepicture, HIGH);
+      while(!digitalRead(moverobot)){};
+      MsTimer2::start();
+      digitalWrite(takepicture, LOW);
+     }
+     
   left_sensor_state = analogRead(left_sensor_pin);
   right_sensor_state = analogRead(right_sensor_pin);
   
   if(right_sensor_state < 500 && left_sensor_state > 500)
   {
     Serial.println("turning right");
-  
+ 
     digitalWrite (motorA1,LOW);
     digitalWrite(motorA2,HIGH);                       
     digitalWrite (motorB1,LOW);
     digitalWrite(motorB2,HIGH);
+  }
   
-  
-   }
   if(right_sensor_state > 500 && left_sensor_state < 500)
   {
     Serial.println("turning left");
@@ -72,26 +97,13 @@ void loop() {
   
     if(right_sensor_state > 500 && left_sensor_state > 500){ 
         Serial.println("going forward");
-      digitalWrite(motorA2,HIGH);
-    digitalWrite(motorA1,LOW);                       
-    digitalWrite(motorB2,LOW);
-    digitalWrite(motorB1,HIGH);
+        digitalWrite(motorA2,HIGH);
+        digitalWrite(motorA1,LOW);                       
+        digitalWrite(motorB2,LOW);
+        digitalWrite(motorB1,HIGH);
       }
 
-
-
-     if(millis() % 3000 == 0){
-      Serial.println("stop motion for picture");
-      digitalWrite(motorA2,LOW);
-      digitalWrite(motorA1,LOW);                       
-      digitalWrite(motorB2,LOW);
-      digitalWrite(motorB1,LOW);
-      delay(turn_delay);
-      
-      digitalWrite(takepicture, HIGH);
-      while(!digitalRead(moverobot));
-      digitalWrite(takepicture, LOW);
-     }
+   
 
 }
 
